4952325aa42314caf1323cd8b449ab41eafa45e2
Your Name <you@example.com>
2025-01-25 11:37:16 +0530

    hai

---
+++ add.py
@@ -0,0 +1,19 @@
+# This program adds two numbers
+numl =
+1.5
+num2 =
+6.3
+# Add two numbers
+sum = numl + num2
+# Display the sum
+print('The sum of {0} and {1} is {2}' . format(numl,
+num2 ,
+sum)[A[A[A[A[A[A[A[A[A[C[C[D[D[D[1[C[3~[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[[A[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[3~[C[C[C[C[C[C[A[C[C[C[[num1 = 1.5
+num2 = 7.6
+
+#Add two numbers
+sum = num1 + num2
+
+#Display the sum
+print()[D'The sum if {0} and {1} is {2}'. format(num1, num2, sum)
+
